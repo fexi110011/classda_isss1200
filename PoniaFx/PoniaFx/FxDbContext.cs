acb5d3a1d2097fb407e4a6c9972bdbfc0cfbacb1
fexi110011 <fakhraddinaa-ab109@code.edu.az>
2025-05-01 13:30:21 +0400

    poina

---
+++ PoniaFx/PoniaFx/FxDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PoniaFx.Models;
+
+namespace PoniaFx
+{
+    public class FxDbContext:DbContext
+    {
+        public FxDbContext(DbContextOptions<FxDbContext> options):base(options) { }
+       
+
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+    }
+}
---
+++ PoniaFx/PoniaFx/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace PoniaFx.Models
+{
+    public class Product:BaseEntity
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public List<ProductImage>? Images { get; set; }
+
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
